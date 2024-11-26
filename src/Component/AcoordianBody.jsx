76ccdaef59314d8cb588ef4caa5935e6b6ae1bfb
gaurya97 <119463762+gaurya97@users.noreply.github.com>
2024-11-26 14:55:45 +0000

    update add cart feature

--- src/Component/AcoordianBody.jsx
+++ src/Component/AcoordianBody.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import image from "../assets/Images/image.png";
-import { addItems } from '../AppStore/CartSlice';
+import { addItems,removeItem } from '../AppStore/CartSlice';
 import { MdCurrencyRupee } from "react-icons/md";
 import { useDispatch } from 'react-redux';
 const AcoordianBody = ({item,imageId,name,price,desc,rating,count}) => {
-
+const [AddedItemCount , setAddedItemCount] = useState(0);
+console.log(AddedItemCount);
   const dispatch =useDispatch();
   const HandleAddItems =()=>{
+    setAddedItemCount(AddedItemCount+1)
 dispatch(addItems(item))
   }
+const AddToLocalState =()=>{
+  setAddedItemCount(AddedItemCount+1)
+  HandleAddItems();
+
+}
+const DeleteToLocalState =()=>{
+  setAddedItemCount(AddedItemCount-1)
+  dispatch(removeItem())
+}
+
   return (
     <div>
       <div className="p-4 max-w-screen-lg mx-auto">
@@ -30,9 +42,9 @@ dispatch(addItems(item))
                 <span className="ml-2 text-sm text-gray-500">({count?count:"No Rating"})</span>
             </div>
             <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${imageId}`} className="w-full h-32 object-cover rounded-lg mb-4" alt="logo" />
-            <button onClick={HandleAddItems}  className="bg-green-500 text-white w-3/5 py-2 my-2 rounded-lg hover:bg-green-600">Add</button>
+            <button className="bg-green-500 text-white w-3/5 py-2 my-2 rounded-lg hover:bg-green-600">{AddedItemCount > 0 ? <span className='flex items-center justify-around'><p onClick={DeleteToLocalState}>-</p>{AddedItemCount}<p onClick={AddToLocalState} >+</p></span>:<div onClick={HandleAddItems} >Add</div>}</button>
         </div>
-
+        {/* <p>-<p/>AddedItemCount<p>+<p/> */}
     {/* </div> */}
 </div>
     </div>
